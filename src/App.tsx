289832ba344cc3b088d7924b9aa5a67d5d8029cf
enesi sefa <enesisefa380@gmail.com>
2022-08-15 21:40:53 +0200

    added search

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ type ITodo = {
 
 function App() {
   const [todos, setTodos] = useState<ITodo[]>([]);
+  const [search, setSearch] = useState("");
+  const [ searchedTodos , setSearchedTodos] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:4000/todos")
@@ -43,6 +45,44 @@ function App() {
     const LastTodos: ITodo[] = [...newTodos, todo];
     setTodos(LastTodos);
   }
+  function updateTodos(todo: ITodo) {
+    let updatedTodo: ITodo = {
+      id: todo.id,
+      todo: todo.todo,
+      // completed: !todo.completed,
+      completed:
+        todo.completed === true
+          ? false
+          : todo.completed === false
+          ? true
+          : true,
+    };
+    console.log(updatedTodo);
+    fetch(`http://localhost:4000/todos/${todo.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTodo),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        let newTodos: ITodo[] = structuredClone(updatedTodo);
+        //@ts-ignore
+        newTodos[todo.id].completed = data.completed;
+
+        setTodos(newTodos);
+      });
+
+    // const findTodo: ITodo = newTodos.find((item) => item.id === todo.id);
+    // const newUpdatedTodo: ITodo = {
+    //   id: todos[todos.length - 1]?.id! + 1,
+    //   todo: todo.todo,
+    //   completed: !todo.completed,
+    // };
+
+    // setTodos(newTodos);
+  }
   function deletingTodo(todo: ITodo) {
     console.log(todo);
     fetch(`http://localhost:4000/todos/${todo.id}`, {
@@ -58,6 +98,12 @@ function App() {
     setTodos(lastTodos);
   }
 
+  function filteredTodos() {
+    const todosClone = structuredClone(todos);
+   let filteredTodos = todosClone.filter((todo : ITodo) =>todo.todo.toUpperCase().includes(search.toUpperCase() ));
+    setTodos(filteredTodos)
+  }
+
   return (
     <div className="App">
       <div>
@@ -69,6 +115,17 @@ function App() {
             creatingTodo(event.target.todo.value);
           }}
         >
+          
+          <label htmlFor="">
+            <input
+              type="text"
+              placeholder="search..."
+              onChange={(e) => {
+                setSearch(e.target.value);
+                filteredTodos()
+              }}
+            />
+          </label>
           <label htmlFor="">
             <input type="text" name="todo" />
             <button>Create</button>
@@ -76,7 +133,13 @@ function App() {
         </form>
         <ul>
           {todos.map((todo) => (
-            <li key={todo.id}>
+            <li
+              onClick={() => {
+                updateTodos(todo);
+              }}
+              className={``}
+              key={todo.id}
+            >
               {todo.todo}
               <button
                 onClick={() => {
